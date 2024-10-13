c8f3fd75e02f07212309d5714e50f75aebebc1f6
Sugarcane Zou <jzou89260@gmail.com>
2024-10-13 03:11:01 +0000

    fix round in couple places

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -9,7 +9,7 @@ def calculate_demographic_data(print_data=True):
     race_count = df['race'].value_counts()
 
     # What is the average age of men?
-    average_age_men = df[df['sex'] == 'Male']['age'].mean().round(2)
+    average_age_men = df[df['sex'] == 'Male']['age'].mean().round(1)
 
     # What is the percentage of people who have a Bachelor's degree?
     percentage_bachelors = df[df['education'] == 'Bachelors']['education'].value_counts() / len(df)
@@ -23,15 +23,15 @@ def calculate_demographic_data(print_data=True):
     lower_education = ~df['education'].isin(['Bachelors', 'Masters', 'Doctorate'])
 
     # percentage with salary >50K
-    higher_education_rich = len(df[higher_education & (df['salary'] == '>50K')]) / len(df[higher_education])
-    lower_education_rich = len(df[lower_education & (df['salary'] == '>50K')]) / len(df[lower_education])
+    higher_education_rich = round(len(df[higher_education & (df['salary'] == '>50K')]) / len(df[higher_education]), 1)
+    lower_education_rich = round(len(df[lower_education & (df['salary'] == '>50K')]) / len(df[lower_education]), 1)
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
     min_work_hours = df['hours-per-week'].min()
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
     num_min_workers = df['hours-per-week'] == min_work_hours
 
-    rich_percentage = df[num_min_workers & (df['salary'] == ">50K")]['salary'].value_counts() / len(df)
+    rich_percentage = round(len(df[num_min_workers & (df['salary'] == '>50K')]) / len(df[num_min_workers]), 1)
 
     # What country has the highest percentage of people that earn >50K?
     highest_earning_country = (df[df['salary'] == ">50K"]['native-country'].value_counts() / df['native-country'].value_counts()).sort_values(ascending = False).reset_index().iloc[0,0]
